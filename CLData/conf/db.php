a943baaaa1cd9b49cd251970b4e7193deadf5410
Josh Carlson <magneticstain@gmail.com>
2016-09-25 19:24:09 -0400

    Commit to Remove Configs from Tracking

--- CLData/conf/db.php
+++ CLData/conf/db.php
@@ -11,11 +11,11 @@ namespace CLTools\CLData;
 	 *  db.php - configuration file containing an array of database metadata needed for db transactions
 	 */
 
-	$DB_CONFIG_OPTIONS = array(
-		'dbUser' => '',
+	$DB_CONFIG_OPTIONS = [
+		'dbUser' => 'cltools',
 		'dbPass' => '',
 		'dbHost' => '127.0.0.1',
 		'dbPort' => 3306,
 		'dbName' => 'cltools'
-	);
+	];
 ?>
