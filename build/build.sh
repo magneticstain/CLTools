bcdb2aea90dae7a8c87fac438062422bdf8677b9
Josh Carlson <magneticstain@gmail.com>
2016-11-29 19:10:49 -0500

    Update Travis CI Config
    Additional build components have been added in order to help facilitate automated build testing.

--- build/build.sh
+++ build/build.sh
@@ -8,9 +8,21 @@
 APP_DIR="/opt/cltools/"
 LOG_DIR="/var/log/cltools/"
 
+# create directories
 echo "Creating application directory..."
 mkdir $APP_DIR > /dev/null 2>&1
 
-
 echo "Creating log directory..."
 mkdir $LOG_DIR > /dev/null 2>&1
+
+# setup database
+echo "Creating database..."
+mysql -u root -e "create database cltools"
+echo "Import table schema..."
+mysql -u root cltools < ../install/cltools.sql
+
+# run any tests
+
+echo "Build complete!"
+
+exit 0
